6eb3e8207caf27e4bb56745766b2617933ef3403
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2016-02-09 11:38:37 +0100

    add divide WIP

--- src/Playground.java
+++ src/Playground.java
@@ -34,13 +34,62 @@ public class Playground {
     }
     
     Binary2dField divide(boolean[][] fieldArray) {
-    	// if (fieldArray len == 1)
-    	// return new Binary2dField( value );
+    	int height = fieldArray.length;
+    	int width = fieldArray[0].length;
     	
-    	int length = fieldArray.length;
-//    	int width = fieldArray[0].wid
-//    	if(fieldArray.)
-    	return null;
+    	 if (height == 1 && width == 1) {
+    		 return null;
+//    		 return new Binary2dField( value );
+    	 }
+    	
+    	Direction direction;
+    	Binary2dField field1, field2;
+		if(height > width) {
+    		direction = Direction.VERTICAL;
+//    		int arrSize = height * width;
+    		int midwayPoint = height/2;
+    		boolean[][] fieldArrayHalf1 = new boolean[midwayPoint][width];
+    		boolean[][] fieldArrayHalf2 = new boolean[height - midwayPoint][width];
+    		
+    		
+//    		System.arraycopy(fieldArray, 0, fieldArrayHalf1, 0, arrSize/2);
+//    		System.arraycopy(fieldArray, arrSize/2, fieldArrayHalf2, 0, arrSize - arrSize/2);
+    		for(int i = 0; i < midwayPoint; ++i) {
+    			for(int j = 0; j < width; ++j) {
+    				fieldArrayHalf1[i][j] = fieldArray[i][j];
+    			}
+    		}
+    		for(int i = height - midwayPoint; i < height; ++i) {
+    			for(int j = 0; j < width; ++j) {
+    				fieldArrayHalf2[i - (height - midwayPoint)][j] = fieldArray[i][j];
+    			}
+    		}
+    		
+    		field1 = divide(fieldArrayHalf1);
+    		field2 = divide(fieldArrayHalf2);
+    	} else {
+    		direction = Direction.HORIZONTAL;
+    		
+    		int midwayPoint = width/2;
+    		boolean[][] fieldArrayHalf1 = new boolean[height][midwayPoint];
+    		boolean[][] fieldArrayHalf2 = new boolean[height][width - midwayPoint];
+    		
+    		for(int i = 0; i < height; ++i) {
+    			for(int j = 0; j < midwayPoint; ++j) {
+    				fieldArrayHalf1[i][j] = fieldArray[i][j];
+    			}
+    		}
+    		for(int i = 0; i < height; ++i) {
+    			for(int j = width - midwayPoint; j < width; ++j) {
+    				fieldArrayHalf2[i][j - (width - midwayPoint)] = fieldArray[i][j];
+    			}
+    		}
+    		
+    		field1 = divide(fieldArrayHalf1);
+    		field2 = divide(fieldArrayHalf2);
+    	}
+    	
+    	return merge(field1, field2, direction);
     }
     
     Binary2dField merge(Binary2dField field1, Binary2dField field2, Direction direction) {
