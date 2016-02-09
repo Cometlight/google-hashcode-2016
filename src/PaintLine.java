ffb881f83a2289d25db0c12df5e4b4897760de55
U-ANS\simon_000 <simon.angerer@hotmail.com>
2016-02-09 11:13:18 +0100

    moves

---
+++ src/PaintLine.java
@@ -0,0 +1,29 @@
+
+public class PaintLine extends Move{
+
+	public int C1;
+	public int R1;
+	
+	public int C2;
+	public int R2;
+	
+	public PaintLine(int C1, int R1, int C2, int R2) {
+		super(1);
+		this.C1 = C1;
+		this.R1 = R1;
+		this.C2 = C2;
+		this.R2 = R2;
+	}
+
+	@Override
+	public void apply(Binary2dField binary2dField) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String toString() {
+		return "PAINT_LINE " + this.R1+ " " + this.C1 + " " + this.R2 + " " + this.C2; 
+	}
+
+}
