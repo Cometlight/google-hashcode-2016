4143ed85801ce2ce8b246df7cfa79c5afa3823a3
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2016-02-11 20:26:52 +0100

    add warehouse manager

--- src/WarehouseManager.java
+++ src/WarehouseManager.java
@@ -2,9 +2,25 @@
  * Created by Zopo on 11.02.2016.
  */
 public class WarehouseManager {
-
+	Simulation _simulation;
+	
     public Warehouse getClosestWarehouse(Coordinate curLocation, ProductType product, int amount){
-        //TODO
-        return null;
+    	Warehouse bestWarehouse = null;
+    	int shortestDistance = Integer.MAX_VALUE;
+    	int highestAmount = 0;
+    	
+    	for (Warehouse warehouse : _simulation._warehouses) {
+    		int amountInWarehouse = warehouse._stock.get(product);
+    		if (amountInWarehouse >= highestAmount) {
+    			int distanceToWarehouse = Utils.getDistance(curLocation, warehouse._location);
+    			if (distanceToWarehouse < shortestDistance) {
+    				bestWarehouse = warehouse;
+    				shortestDistance = distanceToWarehouse;
+    				highestAmount = Math.min(amount, amountInWarehouse);	// We don't need more than amount
+    			}
+    		}
+    	}
+    	
+        return bestWarehouse;
     }
 }
