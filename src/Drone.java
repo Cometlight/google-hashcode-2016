6ea99a40421b2a5cba290c5d49bb9079dd5500fa
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2016-02-11 21:05:40 +0100

    add id to drone

--- src/Drone.java
+++ src/Drone.java
@@ -1,18 +1,35 @@
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Created by Zopo on 11.02.2016.
  */
 public class Drone {
+	int _id;
     Coordinate _curLocation;
     int _maxWeight;
     List<ProductType> _productStorage;
     Simulation _simulation; // needed to inform about actions taken, e.g. "load"
-    Delivery _curDelivery;
-    int _remainingBusyTime;
+    List<Delivery> _deliveries = new LinkedList<>();	// FIFO
+    int _remainingBusyTime = 0;
+    boolean _active = true;
 
     public void performTimestep(){
-        //TODO
+    	if (!_active) {
+    		
+    	}
+    	
+        if (_remainingBusyTime > 0) {
+        	// Drone is still doing something, like flying around like a maniac
+        	--_remainingBusyTime;
+        	return;
+        }
+        
+        if (_deliveries.isEmpty()) {
+        	// Drone needs a new delivery
+        	_deliveries.add(Simulation.getInstance().getNextDelivery(_curLocation));
+        	
+        }
     }
 
     public int getCurrentWeight() {
